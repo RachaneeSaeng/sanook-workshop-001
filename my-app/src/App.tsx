f1861d4f670caf89a4d7d798a11544b9a57216e0
RachaneeSaengkrajai <rachanee.saeng@gmail.com>
2019-04-23 21:25:15 +0700

    React Hook + from

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,24 +1,51 @@
-import React from 'react';
+import React, {Component, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 const App: React.FC = () => {
+  
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [message, setMessage] = useState("")
+
+  const submitForm = (ev: React.FormEvent<HTMLFormElement>) => {
+    ev.preventDefault();
+    alert(`Hi ${name}
+    Email ${email}
+    Message ${message}`
+    );
+    setName('')
+    setEmail('')
+    setMessage('')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Fill me please!</h1>
+      <form onSubmit={submitForm}>
+        <input 
+        type="text" 
+        placeholder="Name" 
+        value={name} 
+        onChange={e => setName(e.target.value)}
+        className="form-field"/>        
+        
+        <input 
+        type="email" 
+        placeholder="Email"
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+        className="form-field"/>
+        
+        <textarea 
+        rows={5} 
+        placeholder="Message" 
+        value={message}
+        onChange={e => setMessage(e.target.value)}
+        className="form-field"/>
+        
+        <button type="submit"  >Submit</button>
+      </form>
     </div>
   );
 }
